fc66fc496c7504d39a60887fd90313ccf4cbd999
Emma Nasseri <emmanasseri@Emmas-Air.midcoip.net>
2024-04-14 06:14:32 -0500

    added demo video

--- src/pages/tap.jsx
+++ src/pages/tap.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import { Box, Text, Center, VStack } from "@chakra-ui/react";
+import { Box, Text, Center, VStack, Button, Link } from "@chakra-ui/react";
 
 const Tap = () => {
   return (
     <Center mt="5vh">
-      {" "}
-      {/* Centers the content vertically and horizontally */}
       <VStack spacing={4}>
-        {" "}
-        {/* Vertical Stack for vertical alignment of children */}
         <Text fontSize="2xl" fontWeight="bold" textAlign="center">
           Tap art to buy it
         </Text>
@@ -22,6 +18,12 @@ const Tap = () => {
         <Text textAlign="center" mt="10vh">
           Please always remember to be gentle and respectful of the art!
         </Text>
+        {/* Button that links to a YouTube video */}
+        <Link href="https://www.youtube.com/watch?v=5C2hr5lEy5E" isExternal>
+          <Button colorScheme="teal" variant="solid">
+            See a Demo
+          </Button>
+        </Link>
       </VStack>
     </Center>
   );
