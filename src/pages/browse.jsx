2276c4cb52ff205643538bb05478219bf52a0898
Emma Nasseri <emmanasseri@Emmas-MacBook-Air.local>
2024-04-14 04:05:16 -0500

    ad text to browse

--- src/pages/browse.jsx
+++ src/pages/browse.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import ArtListing from "../components/ArtListing";
 
 const Browse = () => {
